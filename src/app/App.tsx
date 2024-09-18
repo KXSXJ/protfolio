90b9746bb0f1bc3215b48dee6645a78331a4a594
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-09-18 14:55:04 +0900

    09.18 시멘틱 마크업 작업 #1

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../widgets/header";
+import {Contents} from "../widgets/contents"
+
+export const App :React.FC =()=> {
+  return (
+    <div>
+      <Header/>
+      <Contents/>
+    </div>
+  );
+}
+
